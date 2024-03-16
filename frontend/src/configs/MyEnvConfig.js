1166f235c0b3fc7fe6051baaa36583eff5e8e693
unknown <firedragon7210@gmail.com>
2024-03-16 09:11:07 -0400

    .

---
+++ frontend/src/configs/MyEnvConfig.js
@@ -0,0 +1,19 @@
+export default {
+  baseurl: {
+    dev: '127.0.0.1',
+    prod: 'YOUR_PROD_BASEURL'
+  },
+  stripe: {
+    pbKey: 'pk_test_51JY90jITPrbxGSMcuo8bhxqQhCbSvHghLQaYIxtqVSe9u2xxm80SDtIVQ9acsLTW4WyPJX5G0nIMxaLXwtXbsN0N00vkBYmYDU'
+  },
+  bearer: {
+    tokenPrefix: 'DrapeFit_New'
+  },
+  gcaptcha: {
+    siteKey: 'YOUR_GOOGLE_RECAPTCHA_SITEKEY'
+  },
+  aws: {
+    region: 'ap-south-1',
+    s3Bucket: 'new-drapefit-bucket'
+  }
+};
